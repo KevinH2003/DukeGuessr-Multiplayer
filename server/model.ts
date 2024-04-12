5e849178c2a3b9881a8e2aa768c57cec1bfde6a6
Kevin Huang <klh124@duke.edu>
2024-04-12 16:37:55 -0400

    added types basic infrastructure

--- server/model.ts
+++ server/model.ts
@@ -1,9 +1,66 @@
 export const MODES = ["west", "east", "gardens", "all"]
+export const PHASES = ["guessing", "score-display", "game-over"]
 
 export type Mode = typeof MODES[number]
+export type Phase = typeof PHASES[number]
+export type Player = string
+export type Time = number
 
-export interface GameState {
+export type Coordinates = {
+    lat: number
+    long: number
+    elev: number
+}
 
+export type Guess = {
+    timeSubmitted: Time
+    coords: Coordinates
+}
+
+export type GuessInProgress = Guess | null
+
+export interface StrippedLocation {
+    locationId: number
+    imageUrl: string
+}
+
+export interface Location extends StrippedLocation {
+    coords: Coordinates
+    eligibleModes: Mode[]
+}
+
+export interface GameSetup {
+    players: Player[]
+    mode: Mode
+    locations: Location[]
+    numRounds: number
+    numPlayers: number
+    roundLength: Time
+}
+
+export interface GameState extends GameSetup{
+    round: number
+    playerScores: number[]
+    playerGuesses: GuessInProgress[]
+    phase: Phase
+    roundStart: Time
+    currTime: Time
+}
+
+export function determineWinner(state: GameState): Player{
+    const { players, playerScores } = state;
+
+    // find the index of the maximum score
+    const maxScoreIndex = playerScores.reduce((maxIndex, score, currentIndex) => {
+        if (score > playerScores[maxIndex]) {
+            return currentIndex; 
+        } else {
+            return maxIndex;
+        }
+    }, 0);
+
+    // return the player corresponding to the index of the maximum score
+    return players[maxScoreIndex];
 }
 
 export function createEmptyGame(players: string[], mode: Mode, num_rounds: number): GameState {
